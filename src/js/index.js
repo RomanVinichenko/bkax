35d6e5263fe0246854dbd56b049df57edec3cc0b
r.vinichenko <r.vinichenko@disoft.dev>
2023-03-03 17:35:09 +0200

    Change 03.03.2023(1)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
-const sum = (a, b) => a + b;
-window.sum = sum;
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper.css';
+// import 'swiper/css/navigation';
+
+const swiperNews = new Swiper('.news__swiper', {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation]
+});
+const swiperMedia = new Swiper('.media__swiper', {
+  slidesPerView: 5,
+  spaceBetween: 24,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation]
+});
